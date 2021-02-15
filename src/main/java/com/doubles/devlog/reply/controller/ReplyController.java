a0f43966b4a6405ec836fa53298e997a1bbc0242
Hunson-Yang <iamyhs15@gmail.com>
2021-02-15 18:50:04 +0900

    댓글 불러오기 에러 해

--- src/main/java/com/doubles/devlog/reply/controller/ReplyController.java
+++ src/main/java/com/doubles/devlog/reply/controller/ReplyController.java
@@ -102,7 +102,7 @@ public class ReplyController {
 
             Map<String, Object> map = new HashMap<>();
             map.put("replies", replies);
-            map.put("pageMake", pageMaker);
+            map.put("pageMaker", pageMaker);
 
             entity = new ResponseEntity<>(map, HttpStatus.OK);
 
